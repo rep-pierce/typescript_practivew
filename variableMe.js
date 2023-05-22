f9da54e77c78ba9ddfcae2856448f83ccdab53cc
Ryan Pierce <pierce.ryan.97@gmail.com>
2023-05-22 16:49:35 -0600

    update

--- variableMe.js
+++ variableMe.js
@@ -1,2 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var greetings = "Hello Ryan";
+greetings.toLowerCase();
 console.log(greetings);
+var userID = 334455.3;
+userID.toFixed();
+var loggedIn = false;
--- variableMe.ts
+++ variableMe.ts
@@ -1,5 +1,12 @@
 let greetings: string = "Hello Ryan";
 
+greetings.toLowerCase();
+
 console.log(greetings);
 
-export {}
+export {};
+
+let userID = 334455.3;
+userID.toFixed();
+
+let loggedIn: boolean = false;
